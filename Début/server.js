b15f9a25c27efff6e798739f563870c39e68c9f3
RavioliMuyCalor <fabien.alexandrine@gmail.com>
2018-03-23 16:01:10 +0100

    Ajout de la génération de queries avec les params de l'api

--- Début/server.js
+++ Début/server.js
@@ -14,6 +14,10 @@ async function perform_match_query(res,req_query){
 	//DEFAULT QUERY
 	var query = {match_all:{}};
 	var must = [];
+	var must_not = [];
+	var should = [];
+	var filter = [];
+
 	var i = 0;
 
 	if(req_query!=={}){
@@ -27,17 +31,71 @@ async function perform_match_query(res,req_query){
 			{
 				console.log("//////////////LE X //////////////////////////");
 				//var str = '{"'+x+'":"'+req_query[x]+'"}';
-				var str = '("'+x+'":"'+req_query[x]+'")';
-				//var jsonobj = JSON.parse(str);
-				var jsonobj = str;
 
-				if(req_query[x+"Mode"] == 1)
-				{	
-					jsonobj = "NOT " + jsonobj;
-					console.log(jsonobj);
+				console.log(x + " : " + req_query[x] + " - Mode : " + req_query[x+"Mode"]);
+
+
+		/*		if(req_query[x+"Mode"] == 3)
+				{
+						
+						var str = '{"'+x+'":'+ '{"gt":' +req_query[x]+'}}';
+						var jsonobj = JSON.parse(str);	
+						console.log(jsonobj);
+						filter.push({"range": jsonobj})
+				}*/
+
+				switch(req_query[x+"Mode"]) {
+
+					case "0" : // MUST
+						var str = '{"'+x+'":"'+req_query[x]+'"}';
+						var jsonobj = JSON.parse(str);
+						must.push({"match":jsonobj})
+						console.log("MUST DONE");
+					break;
+
+					case "1" : // MUST_NOT
+						var str = '{"'+x+'":"'+req_query[x]+'"}';
+						var jsonobj = JSON.parse(str);
+						must_not.push({"match":jsonobj})
+					break;
+
+					case "2" : // SHOULD
+						var str = '{"'+x+'":"'+req_query[x]+'"}';
+						var jsonobj = JSON.parse(str);
+						should.push({"match":jsonobj})
+						console.log("SHOULD DONE");
+					break;
+
+					case "3" : // FILTER >
+						var str = '{"'+x+'":'+ '{"gt":' +req_query[x]+'}}';
+						var jsonobj = JSON.parse(str);
+						console.log("FILTER GT DONE");
+						filter.push({"range": jsonobj})
+					break;
+
+					case "4" : // FILTER >=
+						var str = '{"'+x+'":'+ '{"gte":' +req_query[x]+'}}';
+						var jsonobj = JSON.parse(str);
+						console.log("FILTER GTE DONE");
+						filter.push({"range": jsonobj})
+					break;
+
+					case "5" : // FILTER <
+						var str = '{"'+x+'":'+ '{"lt":' +req_query[x]+'}}';
+						var jsonobj = JSON.parse(str);
+						console.log("FILTER LT DONE");
+						filter.push({"range": jsonobj})
+					break;
+
+					case "6" : // FILTER <=
+						var str = '{"'+x+'":'+ '{"lte":' +req_query[x]+'}}';
+						var jsonobj = JSON.parse(str);
+						console.log("FILTER LTE DONE");
+						filter.push({"range": jsonobj})
+					break;
+
 				}
 
-				must.push({"match":jsonobj})
 				i = 1;
 			}
 			else
@@ -46,10 +104,15 @@ async function perform_match_query(res,req_query){
 
 		query = {
 			"bool": {
-				"must": must
+				"must": must,
+				"must_not": must_not,
+				"should": should,
+				"filter":filter
 			}
 		}
 	}
+
+	//console.log(query);
 	res.send(query);
 
 	/*client.search({
